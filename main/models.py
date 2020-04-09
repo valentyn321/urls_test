d3b323231eb6099bc40ae4dbc7f0b1ce8fa3c322
Valentyn <vcherkasov321@gmail.com>
2020-04-09 14:20:44 +0300

    Added model, updated admin.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Url(models.Model):
+    title = models.CharField(max_length=128, blank=False)
+    reference = models.CharField(max_length=256, blank=False)
+    description = models.TextField()
