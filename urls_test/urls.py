e2a16fcee457602a40ea50bb032507a44d21a62e
Valentyn <vcherkasov321@gmail.com>
2020-04-09 13:37:02 +0300

    Added base.html

--- urls_test/urls.py
+++ urls_test/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+from main import views as main_views
+
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
+	path('', main_views.home_page, name="home"),
     url(r'^admin/', admin.site.urls),
 ]
