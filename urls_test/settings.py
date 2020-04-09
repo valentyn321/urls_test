17c6c02a535beff33468535005abeb521e48ac47
Valentyn <vcherkasov321@gmail.com>
2020-04-09 15:10:42 +0300

    Update settings, added requirements.txt

--- urls_test/settings.py
+++ urls_test/settings.py
@@ -120,4 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGIN_URL = 'login'
